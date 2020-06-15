fc36cecb919a3a87e7d1f083e57225b718a084d1
Louis Evans <LEvans@secplus.local>
2020-06-15 14:40:41 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Linq;
+using System.Diagnostics;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter password");
+            var pswd = Console.ReadLine();
+            byte[] passBytes;
+            byte[] hashBytes;
+            string hashed;
+
+            foreach(var sha in new HashAlgorithm[] { SHA1.Create(), SHA256.Create(), SHA384.Create(), SHA512.Create() })
+            {
+                using(sha)
+                {
+                    var timer = new Stopwatch();
+                    timer.Start();
+
+                    passBytes = Encoding.UTF8.GetBytes(pswd);
+                    hashBytes = sha.ComputeHash(passBytes);
+
+                    foreach(var i in Enumerable.Range(1, 100000))
+                    {
+                        hashBytes = sha.ComputeHash(hashBytes);
+                    }
+
+                    hashed = string.Concat(hashBytes.Select(b => b.ToString("x2")));
+
+                    timer.Stop();
+
+                    Console.WriteLine(hashed + " in " + TimeSpan.FromTicks(timer.ElapsedTicks).TotalMilliseconds + " milliseconds");
+                }
+            }
+        }
+    }
+}
