6ffc4b595ca1c2330fceeda0e3abd6c37ce6da04
levans <levans@LT5592>
2020-06-24 12:11:30 +0100

    tidied up a bit

--- Program.cs
+++ Program.cs
@@ -4,29 +4,45 @@ using System.Text;
 using System.Linq;
 using System.Diagnostics;
 
-namespace test
+namespace SimpleHashingExample
 {
     class Program
     {
+        private const int HashCount = 100000;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("enter password");
-            var pswd = Console.ReadLine();
-            byte[] passBytes;
-            byte[] hashBytes;
-            string hashed;
+            var algorithms = new HashAlgorithm[] { 
+                MD5.Create(),
+                SHA1.Create(),
+                SHA256.Create(),
+                SHA384.Create(), 
+                SHA512.Create(),
+            };
 
-            foreach(var sha in new HashAlgorithm[] { SHA1.Create(), SHA256.Create(), SHA384.Create(), SHA512.Create() })
+            Console.WriteLine("Enter a password...");
+
+            var pswd = Console.ReadLine();
+        
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
+
+            foreach(var sha in algorithms)
             {
+                byte[] passBytes;
+                byte[] hashBytes;
+                string hashed;
+
                 using(sha)
                 {
+                    Console.WriteLine("Hashing \"{0}\" {1} time(s) using {2}", pswd, HashCount, sha.GetType().FullName.Split(".").Last().Replace("+Implementation", ""));
+
                     var timer = new Stopwatch();
                     timer.Start();
 
                     passBytes = Encoding.UTF8.GetBytes(pswd);
                     hashBytes = sha.ComputeHash(passBytes);
 
-                    foreach(var i in Enumerable.Range(1, 100000))
+                    foreach(var i in Enumerable.Range(1, HashCount))
                     {
                         hashBytes = sha.ComputeHash(hashBytes);
                     }
@@ -35,7 +51,8 @@ namespace test
 
                     timer.Stop();
 
-                    Console.WriteLine(hashed + " in " + TimeSpan.FromTicks(timer.ElapsedTicks).TotalMilliseconds + " milliseconds");
+                    Console.WriteLine("{0} in {1} milliseconds", hashed, TimeSpan.FromTicks(timer.ElapsedTicks).TotalMilliseconds);
+                    Console.WriteLine("-------------------------------------------------------------------------------------------");
                 }
             }
         }
